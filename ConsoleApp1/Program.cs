01c432adaf3ef183a3820a04235db75d07be8fe5
lygkkk <lygkkk@163.com>
2018-12-13 21:51:54 +0800

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 3;
+            //while (true)
+            //{
+            //    if (i == 0)
+            //    {
+            //        return;
+            //    }
+
+            //    i--;
+            //}
+
+            var node = new TreeNode
+            {
+                Text = "QQ"
+            };
+
+            Test(node);
+        }
+
+        public static void Test(TreeNode node)
+        {
+            var node1 = new TreeNode{Text = "ww"};
+            node.Nodes.Add(node1);
+        }
+    }
+}

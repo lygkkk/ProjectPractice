5c9a832a41e9d74b27743917c21554f1324f1702
Administrator <Administrator@YLMF-2019TNTJJY>
2019-06-26 14:39:39 +0800

    1

--- ProjectPractice.Netcathc-dotnet/Form1.Designer.cs
+++ ProjectPractice.Netcathc-dotnet/Form1.Designer.cs
@@ -30,29 +30,40 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(269, 129);
+            this.pictureBox1.Location = new System.Drawing.Point(72, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(146, 54);
+            this.pictureBox1.Size = new System.Drawing.Size(86, 23);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(421, 145);
+            this.textBox1.Location = new System.Drawing.Point(164, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(382, 74);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ProjectPractice.Netcathc-dotnet/Form1.cs
+++ ProjectPractice.Netcathc-dotnet/Form1.cs
@@ -10,29 +10,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace ProjectPractice.Netcathc_dotnet
 {
     public partial class Form1 : Form
     {
+        private string _cookie = "";
+        private List<string> _loginInfor = null;
         public Form1()
         {
             InitializeComponent();
             this.Load += new EventHandler(Form1_load);
+            this.button1.Click += new EventHandler(btn_Click);
         }
 
         private void Form1_load(object sender, EventArgs e)
         {
-            
+            GetLoginInfo(@"http://sso.ematong.com");
 
-            GetVerificationCode(@"http://sso.ematong.com/captcha.jpg?", GetCookie(@"http://sso.ematong.com"));
+            GetVerificationCode(@"http://sso.ematong.com/captcha.jpg?", _cookie);
 
+        }
 
-            
+        #region 获取登录前的Cookie以及其他信息
 
-            //HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(@"http://sso.ematong.com");
-            //httpWebRequest.Method = "GET";
-            //httpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+        /// <summary>
+        /// 获取登录前的Cookie以及其他信息
+        /// </summary>
+        /// <param name="url">地址</param>
+        /// <returns></returns>
+        private void GetLoginInfo(string url)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.Method = "GET";
+            //httpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
             //httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3704.400 QQBrowser/10.4.3587.400";
             //httpWebRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
             //httpWebRequest.Headers.Add("Accept-Language", "zh-CN,zh;q=0.9");
@@ -40,50 +52,26 @@ namespace ProjectPractice.Netcathc_dotnet
 
             //httpWebRequest.Headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 
-            //HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
-            //var x = response.Headers.GetValues("Set-Cookie")[0].Split(';')[0];
-            //Stream myResponseStream = response.GetResponseStream();
-            //this.pictureBox1.Image = new Bitmap(myResponseStream);
-
-
-            //StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8);
-            //string retString = myStreamReader.ReadToEnd();
-            //myStreamReader.Close();
-            //myResponseStream.Close();
-        }
-
-        #region 获取登录前的Cookie
-
-        /// <summary>
-        /// 获取登录前的Cookie
-        /// </summary>
-        /// <param name="url">地址</param>
-        /// <returns></returns>
-        private string GetCookie(string url)
-        {
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpWebRequest.Method = "GET";
-            httpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
-            httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3704.400 QQBrowser/10.4.3587.400";
-            httpWebRequest.Headers.Add("Accept-Encoding", "gzip, deflate");
-            httpWebRequest.Headers.Add("Accept-Language", "zh-CN,zh;q=0.9");
-            httpWebRequest.Headers.Add("Cache-Control", "max-age=0");
-
-            //httpWebRequest.Headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-
             HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
 
+            Stream responseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(responseStream, Encoding.UTF8);
+            string htmlString = myStreamReader.ReadToEnd();
 
-            HtmlAgilityPack. htmlParse = new HtmlAgilityPack();
-
-            return response.Headers.GetValues("Set-Cookie")[0].Split(';')[0];      
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlString);
+            var nodes = htmlDocument.DocumentNode.SelectNodes("//input[@type='hidden']").ToList();
+            _loginInfor = new List<string>();
+            nodes.ForEach(value => _loginInfor.Add(value.Attributes[1].Value + "=" + value.Attributes[2].Value + "&"));
+            _cookie = (response.Headers.GetValues("Set-Cookie")[0].Split(';')[0]);
+                 
         }
 
         #endregion
 
         #region 获取验证码图片
 
-        private string GetVerificationCode(string url, string cookie)
+        private void GetVerificationCode(string url, string cookie)
         {
             Random random = new Random();
             var g = random.NextDouble();
@@ -92,7 +80,7 @@ namespace ProjectPractice.Netcathc_dotnet
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
             //httpWebRequest.Method = "GET";
             //httpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-            //httpWebRequest.Headers.Add("Cookie", cookie);
+            httpWebRequest.Headers.Add("Cookie", cookie);
             //httpWebRequest.Headers.Add("Referer", "http://sso.ematong.com/login");
             //httpWebRequest.Referer = "http://sso.ematong.com/login";
             //httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3704.400 QQBrowser/10.4.3587.400";
@@ -111,8 +99,6 @@ namespace ProjectPractice.Netcathc_dotnet
             //string retString = myStreamReader.ReadToEnd();
             //myStreamReader.Close();
             //myResponseStream.Close();
-
-            return "1";
            
         }
 
@@ -122,10 +108,38 @@ namespace ProjectPractice.Netcathc_dotnet
 
         private void Login()
         {
-            string account = "15906696262";
-            string pwd = "696262";
-            string verificationCode = textBox1.Text;
+            string account = "username=15906696262&";
+            string pwd = "password=696262&";
+            string verificationCode = $@"yzm={textBox1.Text}";
 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://sso.ematong.com/login?service=http://bm.ematong.com/shiro-cas");
+            request.Method = "POST";
+
+            request.Headers.Add("cookie", _cookie);
+
+            string param = "";
+            _loginInfor.ForEach(val => param += val);
+
+            byte[] paramBytes = Encoding.UTF8.GetBytes(param + account + pwd + verificationCode);
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(paramBytes, 0, paramBytes.Length);
+
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+            string str = streamReader.ReadToEnd();
+
+
+        }
+
+        #endregion
+
+        #region 按钮点击
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Login();
         }
 
         #endregion

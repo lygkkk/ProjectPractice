963e3018e990c958d5d48bfb3ffd258c951eba12
lygkkk <lygkkk@163.com>
2019-07-31 20:20:47 +0800

    ChainPrograming

--- ProjectPractice.Chain/Program.cs
+++ ProjectPractice.Chain/Program.cs
@@ -10,6 +10,33 @@ namespace ProjectPractice.Chain
     {
         static void Main(string[] args)
         {
+            Person person = new Person { Name = "Tom" }
+                .Run()
+                .Sing()
+                .Swim();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Person Run()
+        {
+            Console.WriteLine("Run");
+            return this;
+        }
+
+        public Person Swim()
+        {
+            Console.WriteLine("Swim");
+            return this;
+        }
+
+        public Person Sing()
+        {
+            Console.WriteLine("Sing");
+            return this;
         }
     }
 }

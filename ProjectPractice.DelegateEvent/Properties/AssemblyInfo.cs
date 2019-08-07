129279f35a1db3a0a03303672cd2bde057bb3aaa
Administrator <Administrator@YLMF-2019TNTJJY>
2019-08-07 15:28:03 +0800

    委托事件+entity转json

--- ProjectPractice.DelegateEvent/Properties/AssemblyInfo.cs
+++ ProjectPractice.DelegateEvent/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("单据汇总")]
+[assembly: AssemblyTitle("ProjectPractice.DelegateEvent")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("单据汇总")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("ProjectPractice.DelegateEvent")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("a30b1e1e-bd70-4e83-9461-e03f62cf58dd")]
+[assembly: Guid("00943bac-c6c9-4a66-8c37-a855579adbdd")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
